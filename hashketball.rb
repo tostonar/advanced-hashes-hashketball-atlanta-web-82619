29d7abee9e0593a9e4cbf5cfb97ff27609df18af
Andrea Toston <tostonar@gmail.com>
2019-08-08 01:42:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -244,7 +244,7 @@ def winning_team
   score2 = 0
   score1_points = []
   game_hash[:home][:players].each do |player|
-    score1_points << player[:points] 
+    score1_points << player[:points]
   end
   score1_points
   # game_hash[:away][:players]
@@ -282,5 +282,3 @@ def long_name_steals_a_ton?
   end
   return player_with_most_steals == player_with_longest_name
 end
-
-
